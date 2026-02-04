7bf16a1b927cf8d6c4d2a152ad761d9d30f769bc
Bluethem <david.luza.c@uni.pe>
2026-02-03 20:04:32 -0500

    part 5

--- finder-app/autorun-qemu.sh
+++ finder-app/autorun-qemu.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-cd $(dirname $0)
+
+cd /home || exit 1
+
 echo "Running test script"
 ./finder-test.sh
 rc=$?
+
 if [ ${rc} -eq 0 ]; then
     echo "Completed with success!!"
 else
     echo "Completed with failure, failed with rc=${rc}"
 fi
-echo "finder-app execution complete, dropping to terminal"
-/bin/sh
+
+echo "finder-app execution complete"
